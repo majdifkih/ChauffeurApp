21d7fc4e76eda52a1b0b07fd73bc5eae58081530
majdifkih <majdifkih123@gmail.com>
2022-06-15 18:29:50 +0100

    after code push

--- app-chauf/src/App.js
+++ app-chauf/src/App.js
@@ -8,7 +8,6 @@ import InventoryListe from "./pages/Inventory/Inventory";
 import Login from "./pages/Login/Login";
 import OrderListe from "./pages/Order/Order";
 import InfoFacture from "./pages/Facture/InfoFacture";
-import Test from "./pages/ScanCode/ScanCode";
 
 function App() {
 
@@ -27,7 +26,7 @@ function App() {
            <Route path="inventory" element={<InventoryListe/>}/>
            <Route path="order" element={<OrderListe/>}/>
            <Route path="infofacture" element={<InfoFacture/>}/>
-           <Route path="qrscanner" element={<Test/>}/>
+          
            
             </Route>
         </Routes>
