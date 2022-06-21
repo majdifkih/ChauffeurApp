7858c375ec6da726f6b87f7a3342d1eaf6c7990b
ihs8 <ihebstudent@gmail.com>
2022-06-21 23:47:05 +0200

    commit

--- app-chauf/src/pages/Facture/History.jsx
+++ app-chauf/src/pages/Facture/History.jsx
@@ -29,6 +29,7 @@ import PopupAdd from "../../components/Popup/PopupAdd";
 import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import { Link,useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -52,40 +53,126 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
    
   },
 }));
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
 
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
 function createData(name, date, total, status) {
   return { name, date, total, status};
 }
 
 const rows = [
-  createData('Chocotom','22/01/2022','1.500','done'),
-  createData('SAFIA eau','22/01/2022',' 3.650','undone'),
-  createData('Saida biscuit','22/01/2022',' 7.500 ','done'),
-  createData('Maestro','22/01/2022',' 4000','undone'),
-  createData('Saida','22/01/2022',' 4.100','done'),
-  createData('Crostina','22/01/2022',' 2.700','undone'),
-  createData('Ice cream','22/01/2022',' 1.800','done'),
-  createData('Fidji','22/01/2022',' 800','undone'),
-  createData('Cupcake','22/01/2022',' 900','done'),
-  createData('Chocolat','22/01/2022','700',' undone'),
-  createData('Coca cola','22/01/2022','2000',' done'),
-  createData('Fanta','22/01/2022','1.100',' undone'),
-  createData('Apla','22/01/2022','5.500',' done'),
-  createData('kaki','22/01/2022','4.500 ','undone'),
-  createData('Gaucho ','22/01/2022','2.500',' done'),
+  createData('Chocotom','22/01/2022','1.500','paid'),
+  createData('SAFIA eau','22/01/2022',' 3.650','unpaid'),
+  createData('Saida biscuit','22/01/2022',' 7.500 ','paid'),
+  createData('Maestro','22/01/2022',' 4000','unpaid'),
+  createData('Saida','22/01/2022',' 4.100','paid'),
+  createData('Crostina','22/01/2022',' 2.700','unpaid'),
+  createData('Ice cream','22/01/2022',' 1.800','paid'),
+  createData('Fidji','22/01/2022',' 800','unpaid'),
+  createData('Cupcake','22/01/2022',' 900','paid'),
+  createData('Chocolat','22/01/2022','700',' unpaid'),
+  createData('Coca cola','22/01/2022','2000',' paid'),
+  createData('Fanta','22/01/2022','1.100',' unpaid'),
+  createData('Apla','22/01/2022','5.500',' paid'),
+  createData('kaki','22/01/2022','4.500 ','unpaid'),
+  createData('Gaucho ','22/01/2022','2.500',' paid'),
 ];
  function HistoryListe() {
    const [searchTerm, setSearchTerm] = useState("");
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const navigate = useNavigate();
+  const AddStat = (id,type)=>{
+    if (type==='unpaid') {
+     axios.get(`http://localhost:3001/FactureAPI/fstatus?id=${id}`).then(res=>{
+         if(res.data.success){
+             console.log(res.data.alerts);
+         }
+     }
+     )
+     window.location.href(`/infofacture/:${id}`)
+
+    }
+    
+ }
   useEffect(() => {
     if (!localStorage.getItem('token')) {
      navigate("/login")   
     }
 },[])
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
   
-  
+  const [addPopuphistory, setAddPopuphistory] = useState(false);
+  const [buttonPopup, setButtonPopup] = useState(false);
+  const [factproduct, setfactproduct] = useState("");
   return (
 
   <div className="historymain">
@@ -96,7 +183,7 @@ const rows = [
 <div className="headhistory">
       <div className="titlehistory">
       <div className="headhist">
-          <Link to="/home" style={{textDecoration:"none",color:"#8a8888"}}> Home</Link> <div>-</div> <div>Invoice</div> <div>History</div>
+          <Link to="/" style={{textDecoration:"none",color:"#8a8888"}}> Home</Link> <div>-</div> <div>Invoice</div> <div>History</div>
         </div> 
       <div class="input-icone"><input type="Search" placeholder="Search..." className="rech"/>
       <i><SearchIcon sx={{ fontSize: 40 }}/></i></div>
@@ -116,7 +203,7 @@ const rows = [
         <TableHead>
           <TableRow className="row" >
               
-            <StyledTableCell className="cellprod"   ><input type="radio" className="radio"/>Store</StyledTableCell>
+            <StyledTableCell   ><input type="radio" name="fleet"/>Store</StyledTableCell>
             <StyledTableCell className="cell" >Date</StyledTableCell>
             <StyledTableCell className="cell"  >Total</StyledTableCell>
             <StyledTableCell  className="cell">Status</StyledTableCell>
@@ -137,15 +224,35 @@ const rows = [
             }
           }).map((val,key) => (
             <StyledTableRow className="row" key={key}>
-              <StyledTableCell  width={"20%"} height={"5%"} component="th" scope="row" className="cellprod"><input type="radio" name="fleet" className="radio"/>{val.name}</StyledTableCell>
+              <StyledTableCell  width={"20%"} height={"5%"} component="th" scope="row" className="cellproduct"><input type="radio" name="fleet" className="radio"/>{val.name}</StyledTableCell>
               <StyledTableCell className="cell" >{val.date}</StyledTableCell>
               <StyledTableCell className="cell" >{val.total}</StyledTableCell>
-              <StyledTableCell className={`status ${val.status}`} >{val.status}</StyledTableCell>
-              <StyledTableCell className="cell"><Link to="/infofacture"><i class="material-icons">info_outline</i></Link></StyledTableCell>
+              <StyledTableCell className={`status ${val.status}`} onClick={()=>AddStat(val._id,val.status)} >{val.status}</StyledTableCell>
+             
             </StyledTableRow>
           ))}
           
         </TableBody>
+        <TableFooter >
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={7}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   </div>
